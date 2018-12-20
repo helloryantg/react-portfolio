4ca0a16a3ce57bcaab0a680ef253d5d478b50096
Ryan Gonzales <helloryantg@gmail.com>
2018-12-19 20:44:32 -0800

    add gsap

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import './MainPage.css';
-import NavBar from '../../components/NavBar/NavBar';
+// import NavBar from '../../components/NavBar/NavBar';
+import TweenLite from 'gsap/TweenMax';
 
 class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.myElement = null;
+        this.myTween = null;
+    }
+
+    componentDidMount() {
+        this.myTween = TweenLite.to(this.myElement, 1, {x: 100, y:100});
+    }
+    
     render() {
-        return (
-            <div className="MainPage flexed centered aligned">
-                <NavBar />
-                <p>Hello World!</p>
-            </div> 
-        )    
+        return <div ref={div => this.myElement = div } />;
+        // return (
+        //     <div className="MainPage flexed_column">
+        //         <NavBar />
+        //     </div> 
+        // )    
     }
 }
 
