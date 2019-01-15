d79372d8fece918d8c43d8c5dd73763e3776ffaa
Ryan Gonzales <helloryantg@gmail.com>
2019-01-14 18:51:58 -0800

    Updates

---
+++ precache-manifest.191686fc7d0a101ed2f458f9d4f3170c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7174b4c31764f66e5fb2567d578f6262",
+    "url": "/react-portfolio/static/media/photo-contest.7174b4c3.mp4"
+  },
+  {
+    "revision": "8889838de2e50a9f297dea2d4e3a5586",
+    "url": "/react-portfolio/static/media/fizz-quiz.8889838d.mp4"
+  },
+  {
+    "revision": "7a6f99ab94b6c9b3ae88e7b3ff7cd8d9",
+    "url": "/react-portfolio/static/media/complete-me.7a6f99ab.mp4"
+  },
+  {
+    "revision": "29f2b74c32fb685b1749508ea33566ff",
+    "url": "/react-portfolio/static/media/blackjack-video.29f2b74c.mp4"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "034e81b5138e9cced822",
+    "url": "/react-portfolio/static/js/main.034e81b5.chunk.js"
+  },
+  {
+    "revision": "cf682183aabe8c84fdc1",
+    "url": "/react-portfolio/static/js/1.cf682183.chunk.js"
+  },
+  {
+    "revision": "034e81b5138e9cced822",
+    "url": "/react-portfolio/static/css/main.26f6c021.chunk.css"
+  },
+  {
+    "revision": "acc1db6db50295afe497d9a39f4318ab",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f76a44d813e27cce83c5fe1d06799b67.js"
+  "/react-portfolio/precache-manifest.191686fc7d0a101ed2f458f9d4f3170c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
