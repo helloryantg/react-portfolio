87723414eff33991b3420e6f4ccbb7dda3ae70e4
Ryan Gonzales <helloryantg@gmail.com>
2019-01-14 19:17:19 -0800

    Updates

---
+++ precache-manifest.f76a44d813e27cce83c5fe1d06799b67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7174b4c31764f66e5fb2567d578f6262",
+    "url": "/static/media/photo-contest.7174b4c3.mp4"
+  },
+  {
+    "revision": "8889838de2e50a9f297dea2d4e3a5586",
+    "url": "/static/media/fizz-quiz.8889838d.mp4"
+  },
+  {
+    "revision": "7a6f99ab94b6c9b3ae88e7b3ff7cd8d9",
+    "url": "/static/media/complete-me.7a6f99ab.mp4"
+  },
+  {
+    "revision": "29f2b74c32fb685b1749508ea33566ff",
+    "url": "/static/media/blackjack-video.29f2b74c.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7937b2b0c8acfac090f",
+    "url": "/static/js/main.b7937b2b.chunk.js"
+  },
+  {
+    "revision": "cf682183aabe8c84fdc1",
+    "url": "/static/js/1.cf682183.chunk.js"
+  },
+  {
+    "revision": "b7937b2b0c8acfac090f",
+    "url": "/static/css/main.26f6c021.chunk.css"
+  },
+  {
+    "revision": "021a637e524d1419ee0530129ffd4cf4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.191686fc7d0a101ed2f458f9d4f3170c.js"
+  "/precache-manifest.f76a44d813e27cce83c5fe1d06799b67.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
