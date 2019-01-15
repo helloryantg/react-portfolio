ef8b90b88f2389c42600321aac91f70322cb572b
Ryan Gonzales <helloryantg@gmail.com>
2019-01-14 17:22:34 -0800

    Updates

---
+++ precache-manifest.e77472629c980132dd321b84d64bab62.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7174b4c31764f66e5fb2567d578f6262",
+    "url": "www.ryangonz.com/static/media/photo-contest.7174b4c3.mp4"
+  },
+  {
+    "revision": "8889838de2e50a9f297dea2d4e3a5586",
+    "url": "www.ryangonz.com/static/media/fizz-quiz.8889838d.mp4"
+  },
+  {
+    "revision": "7a6f99ab94b6c9b3ae88e7b3ff7cd8d9",
+    "url": "www.ryangonz.com/static/media/complete-me.7a6f99ab.mp4"
+  },
+  {
+    "revision": "29f2b74c32fb685b1749508ea33566ff",
+    "url": "www.ryangonz.com/static/media/blackjack-video.29f2b74c.mp4"
+  },
+  {
+    "revision": "536949c01f42cca894ce",
+    "url": "www.ryangonz.com/static/js/runtime~main.536949c0.js"
+  },
+  {
+    "revision": "afef1ac2258a56f39121",
+    "url": "www.ryangonz.com/static/js/main.afef1ac2.chunk.js"
+  },
+  {
+    "revision": "de030d7c0d1c065beea9",
+    "url": "www.ryangonz.com/static/js/1.de030d7c.chunk.js"
+  },
+  {
+    "revision": "afef1ac2258a56f39121",
+    "url": "www.ryangonz.com/static/css/main.a1e9aed0.chunk.css"
+  },
+  {
+    "revision": "f45f5af22119ac33d993100913256ab7",
+    "url": "www.ryangonz.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.26f26257065bc71dd9affd4afaf15cd8.js"
+  "www.ryangonz.com/precache-manifest.e77472629c980132dd321b84d64bab62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("www.ryangonz.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
